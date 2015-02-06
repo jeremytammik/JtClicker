6d8ac7cfd71825c0bbe6ff5f681d2cd94fef0bd5
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-02-06 10:20:05 +0100

    recompiled with Visual Studio 2012 in February 2015

--- JtClicker/Properties/AssemblyInfo.cs
+++ JtClicker/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "JtClicker" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "Generic Windows dialogue clicker" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "Autodesk, Inc." )]
 [assembly: AssemblyProduct( "JtClicker" )]
-[assembly: AssemblyCopyright( "Copyright © Jeremy Tammik, Autodesk, Inc. 2009" )]
+[assembly: AssemblyCopyright( "Copyright 2009-2015 © Jeremy Tammik, Autodesk, Inc." )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "1.0.0.1" )]
+[assembly: AssemblyFileVersion( "1.0.0.1" )]
